321e13d690e26a58295968514429b69b74dd4cc0
tlips <thomas.lips@ugent.be>
2019-06-25 22:22:28 +0200

    started on structure

---
+++ server/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import  Config
+
+from os.path import  dirname
+
+
+server_path = dirname(__file__)
+project_path = dirname(server_path)
+template_path = project_path + '/templates'
+
+
+app = Flask(__name__, template_folder=template_path)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+
+
+from server import routes, models
