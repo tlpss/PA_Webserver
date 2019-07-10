0f9bb1f0956ad85245cf39fecc62924829d0d463
tlips <thomas.lips@ugent.be>
2019-07-10 14:33:16 +0200

    finished db and db-tests

--- server/models.py
+++ server/models.py
@@ -2,6 +2,7 @@ from server import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from server import login
+from datetime import datetime
 
 
 # M-N relation
@@ -16,11 +17,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
-    feeders = db.relationship(
-        'Feeder', secondary=FeederUsers,
-        primaryjoin=(FeederUsers.c.user_id == id),
-        secondaryjoin=(FeederUsers.c.feeder_id == id),
-        backref=db.backref('users', lazy='dynamic'), lazy='dynamic')
+    feeders = db.relationship('Feeder', secondary=FeederUsers, backref=db.backref('users', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -38,21 +35,40 @@ def load_user(id):
 
 class Feeder(db.Model):
     id = db.Column(db.Integer, primary_key= True)
-    modified = db.Column(db.Boolean) # meant to signal a change in the schedule to the feeder
     feed_moments = db.relationship('FeedMoment',backref = 'Feeder', lazy= 'dynamic' )
 
     def __repr__(self):
         return f'Feeder {self.id}'
 
+    def get_lastupdate(self):
+        return self.feed_moments.order_by(FeedMoment.last_updated.desc()).all()[0].last_updated
+
+    def get_next_moment(self):
+        now  = datetime.now()
+        current = now.hour*60  +now.minute
+        print(current)
+        result  = self.feed_moments.filter(FeedMoment.feed_time > current).order_by(FeedMoment.feed_time).all()
+        if len(result) > 0:
+            return result[0]
+        else:
+            return self.feed_moments.order_by(FeedMoment.feed_time).all()[0] # last feedmoment of the day , next moment is first of next day
 
 class FeedMoment(db.Model):
-    feeder_id = db.Column(db.Integer, db.ForeignKey('feeder.id'),primary_key = True)
-    timestamp = db.Column(db.Time, primary_key=True, unique=True)
+    feeder_id = db.Column(db.Integer, db.ForeignKey('feeder.id'))
+    feedmoment_id = db.Column(db.Integer, primary_key=True)
+    feed_time = db.Column(db.Integer, default=0)
+    last_updated = db.Column(db.DateTime, default=datetime.now)  # meant to signal a change in the schedule to the feeder
     amount = db.Column(db.Integer)
 
     def __repr__(self):
-        return f'FeedMoment {self.feeder_id} - {self.timestamp}'
+        return f'FeedMoment {self.Feeder} - {self.feed_time}'
 
 
+    def setFeedTime(self,hours, minutes):
+        self.feed_time = hours * 60 + minutes
 
+    def getFeedTime(self):
+        minutes = self.feed_time % 60
+        hours = int ((self.feed_time - minutes) / 60)
+        return {'hours': hours, 'minutes': minutes}
 
--- tests/db_tests.py
+++ tests/db_tests.py
@@ -1,7 +1,7 @@
 import unittest
 from server import app, db
 from server.models import User, Feeder, FeederUsers, FeedMoment
-
+from datetime import datetime
 
 class UserModelCase(unittest.TestCase):
     def setUp(self):
@@ -27,7 +27,10 @@ class UserModelCase(unittest.TestCase):
         feeder = Feeder()
         moment1 = FeedMoment(amount  = 1, Feeder  = feeder)
         moment2 = FeedMoment(amount  =2 ,Feeder  = feeder)
-
+        db.session.add(feeder)
+        db.session.add(moment1)
+        db.session.add(moment2)
+        db.session.commit()
         self.assertTrue(len(feeder.feed_moments.all()) == 2)
         self.assertTrue(feeder.feed_moments[0].amount == 1)
 
@@ -36,12 +39,45 @@ class UserModelCase(unittest.TestCase):
         user1 = User(username="test1")
         user2 = User(username="test2")
 
-        feeder = Feeder(modified=False)
+        feeder = Feeder()
         feeder.users.append(user1)
         user2.feeders.append(feeder)
 
-        self.assertTrue( len(feeder.users.all()) == 2)
+        db.session.add(feeder)
+        db.session.add(user1)
+        db.session.add(user2)
 
+        db.session.commit()
+
+        self.assertTrue( len(feeder.users.all()) == 2)
+        self.assertEqual(user1.feeders[0],feeder)
+
+
+
+    def test_Feederusage(self):
+        user1 = User(username="test1")
+        feeder =  Feeder()
+        #feeder.users.append(user1)
+
+        moment1 = FeedMoment(amount= 2, Feeder=feeder)
+        moment1.setFeedTime(5,12)
+
+        moment2 = FeedMoment(amount= 3, Feeder=feeder)
+        moment2.setFeedTime(14,15)
+
+        db.session.add(feeder)
+        db.session.add(moment1)
+        db.session.add(moment2)
+        db.session.commit()
+        #print(feeder.get_lastupdate())
+        self.assertEqual(moment2.last_updated,feeder.get_lastupdate()) #test global last update
+        #!! current time is important!
+        time = datetime.now().hour*60 + datetime.now().minute
+        print(time)
+        if time > moment2.feed_time:
+            self.assertEqual(feeder.get_next_moment(),moment1)
+        else:
+            self.assertEqual(feeder.get_next_moment(),moment2)
 
 
 
